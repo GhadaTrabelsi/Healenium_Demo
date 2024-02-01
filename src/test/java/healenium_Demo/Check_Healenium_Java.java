e8207be199257cffc7c90461ac22068aab2694de
ghada <tghada0211@gmail.com>
2024-02-01 02:28:27 +0100

    first commit

---
+++ src/test/java/healenium_Demo/Check_Healenium_Java.java
@@ -0,0 +1,32 @@
+package healenium_Demo;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import com.epam.healenium.SelfHealingDriver;
+
+public class Check_Healenium_Java {
+
+	public static void main(String[] args) {
+
+		//declare delegate
+		  WebDriver delegate = new ChromeDriver();
+		  //create Self-healing driver
+		  SelfHealingDriver driver = SelfHealingDriver.create(delegate);
+		  driver.get("https://elenastepuro.github.io/test_env/index.html");
+		  driver.manage().window().maximize();
+		  driver.findElement(By.id("change_id")).sendKeys("My name is Ghada TRABELSI");
+	        driver.findElement(By.className("test_class")).sendKeys("test_class");
+	        driver.findElement(By.xpath("//*[@id=\"Submit\"]/span/i")).click();
+	        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+	        driver.findElement(By.id("change_id")).clear();
+	        driver.findElement(By.className("test_class")).clear();
+	        driver.findElement(By.id("change_id")).sendKeys("Im an automation tester");
+	        driver.findElement(By.className("test_class")).sendKeys("test_new_class");
+	        driver.close();
+	}
+
+}
